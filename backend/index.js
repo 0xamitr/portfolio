73432972383031660091dd4b4b6e0c1290ed6944
0xamitr <amitrathore.aaa@gmail.com>
2023-12-25 08:27:35 +0530

    blog post request

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const Blog = require("./models/model");
 const app = express();
 require("dotenv").config();
-const port = 3000;
+const port = 5000;
 const mongoose = require("mongoose");
+const cors = require('cors');
+const multer = require('multer');
 
+// Enable CORS
+app.use(cors());
+const upload = multer();
 mongoose.set("strictQuery", false);
 
 const mongoDB = process.env.MONGODB_URI;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -19,9 +27,18 @@ app.get('/', async (req, res)=>{
   console.log(blogs);
 });
 
-// app.post('/', (req, res)=>{
-//   const blog = new Blog
-// });
+app.post('/', upload.none(), (req, res)=>{
+  const blog = new Blog({
+    heading: req.body.heading,
+    content: req.body.content,
+    date: new Date()
+  });
+  try{
+    blog.save();
+  }catch{
+    console.log("cant save");
+  }
+});
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
--- frontend/src/app/blog/page.js
+++ frontend/src/app/blog/page.js
@@ -1,6 +1,33 @@
+'use client';
 import "./page.css"
 export default function Blog(){
+    const handleSubmit = async(event)=> {
+        const link = "http://localhost:5000";
+        event.preventDefault();
+        const formData = new FormData(event.target)
+        try{
+            const response = await fetch(link, {
+                method: 'POST',
+                body: formData,
+            })
+            console.log(response);
+        }catch{
+            console.log("failed");
+        }
+        
+    }
     return(
-        <h1>This is a motherfucking blog</h1>
+        <>
+            <h1>This is a motherfucking blog</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input name="heading" type="text"/>
+                </label>
+                <label>
+                    <input name="content" type="text"/>
+                </label>
+                <button id="btn" type="submit">Submit</button>
+            </form>
+        </>
     )
 }
