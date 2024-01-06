2d7cec64633aa652a855397e3180a9c799628f72
0xamitr <0xamitr@gmail.com>
2024-01-07 05:03:23 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 mongoose.set("strictQuery", false);
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://amitrathore.in"); // Allow access from all origins
+  res.header("Access-Control-Allow-Origin", "https://www.amitrathore.in"); // Allow access from all origins
   // You can also specify a specific origin: res.header("Access-Control-Allow-Origin", "https://yourdomain.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
