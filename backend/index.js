4b86700b594d9cb365062a1dc40146e5d9a0794f
0xamitr <0xamitr@gmail.com>
2024-01-07 05:01:04 +0530

    configuring cors

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const port = 5000;
 const app = express();
 mongoose.set("strictQuery", false);
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://amitrathore.in"); // Allow access from all origins
+  // You can also specify a specific origin: res.header("Access-Control-Allow-Origin", "https://yourdomain.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/blog', router);
