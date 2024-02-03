3c5f38e1c03c15231f28857767a15b5293c3ed10
0xamitr <0xamitr@gmail.com>
2024-02-03 23:48:55 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,9 @@ mongoose.set("strictQuery", false);
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/blog', router);
 const mongoDB = process.env.MONGODB_URI;
 
