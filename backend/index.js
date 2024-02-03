9e0ddf80eb485f80950555e96bd6fbe6bac8173e
0xamitr <0xamitr@gmail.com>
2024-02-03 23:51:13 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import cors from 'cors';
 import router from './api/routes/routes.js'
- 
+import bodyParser from 'body-parser';
+
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
